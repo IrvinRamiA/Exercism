1ea2c576227f7bd538e911a16a14ab81837a807d
IrvinRamiA <iramirezaltammx@gmail.com>
2020-05-23 23:44:34 -0500

    Added two_fer.c module

---
+++ c/two-fer/src/two_fer.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "two_fer.h"
+
+void two_fer(char *buffer, const char *name)
+{
+    sprintf(buffer, "One for %s, one for me.", name == NULL ? "you" : name);
+}
